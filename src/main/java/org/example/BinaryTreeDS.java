e6ca2788882e9418ac18c7abbf4104884e03e9dc
Sahid Reza <sahidreza976@gmail.com>
2026-07-19 19:22:03 +0530

    Add inorder

--- src/main/java/org/example/BinaryTreeDS.java
+++ src/main/java/org/example/BinaryTreeDS.java
@@ -27,11 +27,22 @@ public class BinaryTreeDS {
         }
     }
 
+    // InOrder
+    public static void inOrder(Node root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.println(root.data+" ");
+        inOrder(root.right);
+    }
+
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.print(root.data);
+       // System.out.print(root.data);
+        inOrder(root);
 
     }
 }
