de9d8688a9a3c3cbf7df7966b4457ec17224e4e9
Sahid Reza <sahidreza976@gmail.com>
2026-07-19 19:25:19 +0530

    Add PostOrder

--- src/main/java/org/example/BinaryTreeDS.java
+++ src/main/java/org/example/BinaryTreeDS.java
@@ -36,13 +36,22 @@ public class BinaryTreeDS {
         System.out.println(root.data+" ");
         inOrder(root.right);
     }
+    // PostOrder
+    public static void postOrder(Node root){
+        if(root == null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.data+" ");
+    }
 
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
        // System.out.print(root.data);
-        inOrder(root);
-
+       // inOrder(root);
+         postOrder(root);
     }
 }
